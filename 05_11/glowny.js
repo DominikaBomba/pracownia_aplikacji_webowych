6836afcae369ebfde541d2db0c9b3f4b92afb6fa
Dominika Bomba <146015989+DominikaBomba@users.noreply.github.com>
2025-11-05 19:10:25 +0100

    Zadania lekcja 05_11

---
+++ 05_11/glowny.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const fs = require('fs')
+const url = require('url')
+const mysql = require('mysql')
+app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({ extended: true }));
+
+function writehtml(title, content, header) {
+    const html_file = fs.readFileSync('./static/plik.html', 'utf8');
+    return html_file
+        .replace('{{title}}', title)
+        .replace('{{content}}', content)
+        .replace('{{header}}', header);
+}
+
+app.get('/', (req, res) => {
+    res.send(writehtml("<h1>Strona główna</h1>",
+        "<p>Piekarnia to miejsce, gdzie każdego dnia unosi się zapach świeżo wypiekanego chleba, bułek i ciast. </p> <p>Rzemieślnicy z pasją łączą tradycję z nowoczesnością, tworząc wypieki, które cieszą podniebienie i serce.</p>", "Strona główna"))
+})
+app.get('/api/contact-messages', (req, res)=>{
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mysql'
+    })
+    connection.connect()
+    connection.query(`SELECT message FROM messages`, (err, rows, fields) => {
+        if (err) throw err
+        rows.forEach((row)=>{
+            console.log(row.message)
+        })
+        res.send(JSON.stringify({ rows}))
+
+    })
+})
+app.get('/api/contact-messages/:id', (req, res)=> {
+    const id_z_sciezki = req.params.id;
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mysql'
+    })
+    connection.connect()
+    connection.query(`SELECT message FROM messages where id = ${id_z_sciezki}`, (err, rows, fields)=>{
+       res.send(rows[0])
+        if (err) throw res.status(404).send('Błąd:(((')
+    })
+})
+
+
+app.get('/o-nas', (req, res) => {
+
+    const content = `
+        <p>Piekarnia to miejsce, gdzie każdego dnia unosi się zapach świeżo wypiekanego chleba, bułek i ciast.</p>
+        <p>Rzemieślnicy z pasją łączą tradycję z nowoczesnością, tworząc wypieki, które cieszą podniebienie i serce.</p>
+
+        <div>
+            <img src="i1.png" alt="zdjęcie">
+            <img src="i1.png" alt="zdjęcie">
+            <img src="i2.png" alt="zdjęcie">
+        </div>
+
+        <table border="1" cellpadding="8" cellspacing="0">
+            <tr><th>Usługa IT</th><th>Cena</th></tr>
+            <tr><td>Tworzenie stron internetowych</td><td>1500 zł</td></tr>
+            <tr><td>Administracja serwerami</td><td>1000 zł/miesiąc</td></tr>
+            <tr><td>Audyt bezpieczeństwa</td><td>2000 zł</td></tr>
+        </table>
+    `;
+    res.send(writehtml("O nas",
+        content
+        , '<h1> O firmie</h1>'))
+})
+
+app.get('/oferta', (req, res) => {
+    const title = '<h1>Oferta</h1>';
+    const content = `
+        <p>Piekarnia to miejsce, gdzie każdego dnia unosi się zapach świeżo wypiekanego chleba, bułek i ciast.</p>
+        <p>Rzemieślnicy z pasją łączą tradycję z nowoczesnością, tworząc wypieki, które cieszą podniebienie i serce.</p>
+        <table border="1" cellpadding="8" cellspacing="0">
+            <tr><th>Usługa IT</th><th>Cena</th></tr>
+            <tr><td>Tworzenie stron internetowych</td><td>1500 zł</td></tr>
+            <tr><td>Administracja serwerami</td><td>1000 zł/miesiąc</td></tr>
+            <tr><td>Audyt bezpieczeństwa</td><td>2000 zł</td></tr>
+        </table>
+    `;
+    res.send(writehtml(title, content, "Oferta"));
+});
+
+app.get('/kontakt', (req, res) => {
+    const content = `
+        <form action="/kontakt" method="POST">
+            <label for="imie">Imię:</label><br />
+            <input type="text" id="imie" name="imie"  /><br /><br />
+
+            <label for="nazwisko">Nazwisko:</label><br />
+            <input type="text" id="nazwisko" name="nazwisko"  /><br /><br />
+
+            <label for="email">Email:</label><br />
+            <input type="email" id="email" name="email"  /><br /><br />
+
+            <label for="wiadomosc">Treść wiadomości:</label><br />
+            <textarea id="wiadomosc" name="wiadomosc" rows="5" ></textarea><br /><br />
+
+            <button type="submit">Wyślij</button>
+        </form>
+    `;
+    res.send(writehtml("Kontakt", content, "Kontakt"));
+})
+
+app.post('/kontakt', function (req, res) {
+    console.log(req.body);
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'mysql'
+    })
+    connection.connect()
+    connection.query(`INSERT INTO messages (message) VALUES ('${req.body.wiadomosc}') `, (err) => {
+        if (err) throw err
+
+        console.log(req.body)
+    })
+    connection.end();
+
+    res.redirect('/'); 
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
